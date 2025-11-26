6ba14b19f64d62b81310c9103d7ed5f3116572c8
SkamchikWqqq <paranoiaattack1@gmail.com>
2025-11-26 05:19:50 +0200

    Refactor UsersDB and SubscriptionDB into functions

--- database/users_db.py
+++ database/users_db.py
@@ -1,47 +1,36 @@
 # database/users_db.py
 
-class UsersDB:
-    """
-    Минимальная база пользователей.
-    Использует временное хранилище в памяти (set).
-    """
-    def __init__(self):
-        self.users = set()
+# Временное хранилище пользователей и подписок
+_users = set()
+_subscriptions = {}
 
-    def add_user(self, user_id):
-        """Добавляет пользователя"""
-        self.users.add(user_id)
+# --- Пользователи ---
+def add_user(user_id):
+    """Добавляет пользователя"""
+    _users.add(user_id)
 
-    def remove_user(self, user_id):
-        """Удаляет пользователя"""
-        self.users.discard(user_id)
+def remove_user(user_id):
+    """Удаляет пользователя"""
+    _users.discard(user_id)
 
-    def user_exists(self, user_id):
-        """Проверяет, есть ли пользователь"""
-        return user_id in self.users
+def user_exists(user_id):
+    """Проверяет, есть ли пользователь"""
+    return user_id in _users
 
-    def all_users(self):
-        """Возвращает список всех пользователей"""
-        return list(self.users)
+def all_users():
+    """Возвращает список всех пользователей"""
+    return list(_users)
 
+# --- Подписки ---
+def subscribe(user_id):
+    """Подписка пользователя"""
+    _subscriptions[user_id] = True
 
-class SubscriptionDB:
-    """
-    Минимальная база подписок.
-    Временное хранилище user_id -> True/False.
-    """
-    def __init__(self):
-        self.subscriptions = {}
+def unsubscribe(user_id):
+    """Отписка пользователя"""
+    _subscriptions[user_id] = False
 
-    def subscribe(self, user_id):
-        """Подписка пользователя"""
-        self.subscriptions[user_id] = True
-
-    def unsubscribe(self, user_id):
-        """Отписка пользователя"""
-        self.subscriptions[user_id] = False
-
-    def is_subscribed(self, user_id):
-        """Проверяет, подписан ли пользователь"""
-        return self.subscriptions.get(user_id, False)
-      
+def is_subscribed(user_id):
+    """Проверяет, подписан ли пользователь"""
+    return _subscriptions.get(user_id, False)
+    
