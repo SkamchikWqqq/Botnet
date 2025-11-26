10e5add3de0672482882dc639689d4b388f293a3
SkamchikWqqq <paranoiaattack1@gmail.com>
2025-11-26 03:34:35 +0200

    Change TOKEN and add CHANNEL_ID to config
    Updated TOKEN value and added CHANNEL_ID for the private channel.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '8295986577:AAHmheRcMf1iERO8c36WmgUf5IlRXosDfZs'
+TOKEN = 'ТОКЕН_ПОСЛЕ_СМЕНЫ'
 ADMIN = 130231824
 SUPPORT = 't.me/paranoiafsb'
 photo_path = 'image.png'
@@ -6,3 +6,5 @@ API_ID = 22339547
 API_HASH = '6857117d632b8769bbc507ddb357d52d'
 CRYPTOTOKEN = '403239:AA1REXVjgYxtMRELxI5pFOLLIP5swiri2Ke'
 DIRECTORY = 'sessions'
+
+CHANNEL_ID = -1002415070098  # ← твой закрытый канал
