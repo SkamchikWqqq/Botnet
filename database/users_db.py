d9245369b473a51fd75edf658793973466d51a0c
SkamchikWqqq <paranoiaattack1@gmail.com>
2025-11-26 05:24:55 +0200

    Add count_users function to track user count
    Added a function to count the number of users.

--- database/users_db.py
+++ database/users_db.py
@@ -6,31 +6,27 @@ _subscriptions = {}
 
 # --- Пользователи ---
 def add_user(user_id):
-    """Добавляет пользователя"""
     _users.add(user_id)
 
 def remove_user(user_id):
-    """Удаляет пользователя"""
     _users.discard(user_id)
 
 def user_exists(user_id):
-    """Проверяет, есть ли пользователь"""
     return user_id in _users
 
 def all_users():
-    """Возвращает список всех пользователей"""
     return list(_users)
+
+def count_users():
+    """Возвращает количество пользователей"""
+    return len(_users)
 
 # --- Подписки ---
 def subscribe(user_id):
-    """Подписка пользователя"""
     _subscriptions[user_id] = True
 
 def unsubscribe(user_id):
-    """Отписка пользователя"""
     _subscriptions[user_id] = False
 
 def is_subscribed(user_id):
-    """Проверяет, подписан ли пользователь"""
     return _subscriptions.get(user_id, False)
-    
