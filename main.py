6fa414e852282341a0dc0bcca37a05d851775db1
SkamchikWqqq <paranoiaattack1@gmail.com>
2025-11-26 05:33:06 +0200

    Clean up main.py and add newline at EOF
    Removed commented lines and added a newline at the end of the file.

--- main.py
+++ main.py
@@ -10,22 +10,20 @@ storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 async def main():
-    # Создаём бот с дефолтными свойствами
     bot = Bot(
         token=TOKEN,
         default=DefaultBotProperties(parse_mode="HTML")
     )
-    
+
     # Подключаем маршруты
     dp.include_router(router)
-    
+
     # Запуск поллинга
     try:
         await dp.start_polling(bot)
     finally:
-        # Закрываем бот корректно
         await bot.session.close()
 
-# Запуск
 if __name__ == "__main__":
     asyncio.run(main())
+    
